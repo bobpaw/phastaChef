45a9a1cae8782924f635154b15d4143b0c8ebb90
Aiden Woodruff <woodra@rpi.edu>
2024-05-15 14:47:13 -0400

    test: Add spherical shock function test
    Change PlanarShockFunc function to Ax+By+Cz-D <= tol.
    Add spherical function.
    
    Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

--- test/ShockFunc.h
+++ test/ShockFunc.h
@@ -26,7 +26,7 @@ namespace pc {
     };
 
     /**
-     * @brief A 3D Planar shock function |Ax+By+Cz+D|<=tol.
+     * @brief A 3D Planar shock function |Ax+By+Cz-D|<=tol.
      * 
      */
     class PlanarShockFunc : public ShockFunc {
@@ -37,10 +37,27 @@ namespace pc {
     private:
       virtual bool eval(apf::Mesh* m, apf::MeshEntity* e) {
         apf::Vector3 lc = apf::getLinearCentroid(m, e);
-        return std::abs(a * lc.x() + b * lc.y() + c * lc.z() + d) <= tol_;
+        return std::abs(a * lc.x() + b * lc.y() + c * lc.z() - d) <= tol_;
       }
       double a, b, c, d, tol_;
     };
+
+		/**
+		 * @brief A 3D Spherical shock function |(x-A)^2+(y-B)^2+(z-C)^2-R^2|<=tol.
+     */
+		class SphericalShockFunc : public ShockFunc {
+		public:
+			SphericalShockFunc(double A, double B, double C, double R, double tol) :
+			a(A), b(B), c(C), r(R), tol_(tol) {}
+
+		private:
+			virtual bool eval(apf::Mesh* m, apf::MeshEntity* e) {
+				apf::Vector3 lc = apf::getLinearCentroid(m, e);
+				double i = lc.x() - a, j = lc.y() - b, k = lc.z() - c;
+				return std::abs(i * i + j * j + k * k - r * r) <= tol_;
+			}
+			double a, b, c, r, tol_;
+		};
 
 		ShockFunc* ShockFunc::makeFromString(std::string s, double tol) {
 			std::string::size_type col = s.find(':');
@@ -64,6 +81,11 @@ namespace pc {
 					throw std::invalid_argument("invalid ShockFunc arguments");
 				}
 				return new PlanarShockFunc(args[0], args[1], args[2], args[3], tol);
+			} else if (s_type == "sphere") {
+				if (args.size() != 4) {
+					throw std::invalid_argument("invalid ShockFunc arguments");
+				}
+				return new SphericalShockFunc(args[0], args[1], args[2], args[3], tol);
 			} else {
 				throw std::invalid_argument("invalid ShockFunc type");
 			}
