01c122f73a9337f6444abf2621022383d740e5b3
Aiden Woodruff <woodra@rpi.edu>
2024-06-12 09:46:00 -0400

    test: Add prisms cases
    Add through-edge for prisms.
    Add testRay overload that doesn't do checks.
    Add output announcing which ray.
    
    Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

--- test/test_prisms.cc
+++ test/test_prisms.cc
@@ -22,8 +22,8 @@ apf::Mesh2* createMesh(void) {
   gmi_model* g = gmi_load(".null");
   apf::Mesh2* m = apf::makeEmptyMdsMesh(g, 3, false);
 
-  const int prisms = 4 * 2, tets = prisms * 3 + 2, planeverts = 6,
-    verts = planeverts * 3 + 2;
+  const int prisms = 4 * 2, tets = prisms * 3 + 5, planeverts = 6,
+    verts = planeverts * 3 + 5;
 
   double coordsZ0[planeverts * 2] = {
     0, 0, 1, 0, 0, 1, 1, 1, 2, 0, 2, 1
@@ -41,12 +41,21 @@ apf::Mesh2* createMesh(void) {
     coords[(i + planeverts * 2) * 3 + 2] = -1;
   }
 
-  coords[54] = 3;
+  coords[54] = 3; // vert 18, tet 24.
   coords[55] = 1;
   coords[56] = 1;
-  coords[57] = -1;
+  coords[57] = -1; // vert 19, tet 25.
   coords[58] = 0;
   coords[59] = -1;
+  coords[60] = 0; // vert 20, tet 26.
+  coords[61] = 2;
+  coords[62] = 1.5;
+  coords[63] = 0; // vert 21, tet 27.
+  coords[64] = -1;
+  coords[65] = 2;
+  coords[66] = 2; // vert 22, tet 28.
+  coords[67] = 2;
+  coords[68] = 2;
 
   apf::Gid conn[tets * 4] = {
     0,6,7,8,/**/ 0,1,8,7,/**/ 0,1,2,8, // Prism 1
@@ -57,7 +66,8 @@ apf::Mesh2* createMesh(void) {
     13,15,2,3,/**/ 13,15,14,2,/**/ 13,2,1,3, // Prism 6
     13,1,4,3,/**/ 13,16,3,4,/**/ 13,16,15,3, // Prism 7
     16,17,3,5,/**/ 16,17,15,3,/**/ 16,3,4,5, // Prism 8
-    4,16,5,18, /*Start tet*/ 0,8,2,19 /*End tet*/
+    4,16,5,18, /*Tet 24*/ 0,8,2,19, /*Tet 25*/ 2,3,15,20, /*Tet 26*/
+    1,10,7,21, /*Tet 27*/ 3,8,9,22 /*Tet 28*/
   };
 
   apf::GlobalToVert outmap;
@@ -83,6 +93,7 @@ apf::Mesh2* createMesh(void) {
 std::vector<apf::MeshEntity*> tetsById;
 
 void testRay(apf::Mesh* m, const char* fieldname, int start_id, int end_id, const std::vector<int>& steps) {
+  std::cout << "Testing ray " << fieldname << '.' << std::endl;
   apf::Field* ray = apf::createField(m, fieldname, apf::SCALAR, apf::getConstant(3));
   apf::zeroField(ray);
   int step = 0;
@@ -91,7 +102,7 @@ void testRay(apf::Mesh* m, const char* fieldname, int start_id, int end_id, cons
     if (step < steps.size()) {
       if (e != tetsById[steps[step]]) {
         fprintf(stderr, "ERROR: Expected to be at element %p but actually at"
-          "element %p.\n", tetsById[steps[step]], e);
+          " element %p.\n", tetsById[steps[step]], e);
         exit(EXIT_FAILURE);
       }
     } else {
@@ -106,6 +117,21 @@ void testRay(apf::Mesh* m, const char* fieldname, int start_id, int end_id, cons
   }
 }
 
+void testRay(apf::Mesh* m, const char* fieldname, int start_id, int end_id) {
+  std::cout << "Testing ray " << fieldname << '.' << std::endl;
+  apf::Field* ray = apf::createField(m, fieldname, apf::SCALAR, apf::getConstant(3));
+  apf::zeroField(ray);
+  int step = 0;
+  pc::rayTrace(m, tetsById[start_id], tetsById[end_id], [ray, &step](apf::Mesh* m, apf::MeshEntity* e) {
+    apf::setScalar(ray, e, 0, step + 1);
+    if (step > tetsById.size()) {
+      fprintf(stderr, "Error: too many steps. Infinite loop?\n");
+      exit(EXIT_FAILURE);
+    }
+    ++step;
+  });
+}
+
 void testRays(apf::Mesh2* m) {
   apf::Numbering* tet_id = m->findNumbering("tet id");
 
@@ -114,13 +140,18 @@ void testRays(apf::Mesh2* m) {
   for (apf::MeshEntity* e = m->iterate(it); e; e = m->iterate(it)) {
     int id = apf::getNumber(tet_id, e, 0, 0);
     tetsById[id] = e;
-    std::cout << "Tet " << id << ": " << e << std::endl;
+    std::cout << "Tet " << id << ": " << e << ": " <<
+      apf::getLinearCentroid(m, e) << std::endl;
   }
   m->end(it);
 
   testRay(m, "ray1", 10, 8, {10, 8});
   testRay(m, "ray2", 10, 0, {10, 8, 3, 5, 1, 0});
   testRay(m, "ray3", 24, 25, {24, 10, 18, 4, 12, 25});
+  testRay(m, "ray4", 15, 24);
+  testRay(m, "ray5", 26, 17, {26, 17});
+  testRay(m, "ray6", 26, 13, {26, 15, 17, 13});
+  testRay(m, "ray7", 27, 28);
 }
 
 int main (int argc, char* argv[]) {
